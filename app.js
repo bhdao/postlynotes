829b2c4dc1c6a53cac503c7bfc105e3eec071609
bhdao <onthedaolow@gmail.com>
2022-10-06 21:11:45 -0700

    Changes for Heroku Deployment

--- app.js
+++ app.js
@@ -86,6 +86,6 @@ app.use('/notes', require('./routes/notes'))
 
 
 
-const PORT = process.env.PORT || 3000
+let PORT = process.env.PORT || 8000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
